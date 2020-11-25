765df5bcc9dc3b5a1bfa19f306e3e390002d8d81
companion <pandonautical@gmail.com>
2020-11-25 16:05:36 +0000

    added blacklist api

--- little-api-companion/command.js
+++ little-api-companion/command.js
@@ -1,15 +1,11 @@
+require('./blacklist.js')
 module.exports = {
-  name: "test",
-  description: "test api",
+  name: "command",
+  description: "command api",
   mold(message, prefix) {
     args = message.content.slice(prefix.length).trim().split(/ +/);
     command = args.shift().toLowerCase();
     molded = { "args" : args,  "command" : command };
     return molded;
-  },
-  cbl(path, value) {
-    const b = fs.readFileSync(path, 'utf-8');
-    const blacklist = b.split("\n");
-    return blacklist.includes(value);
   }
 }
