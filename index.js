f8b580188690e23077c3d94b31447dc2cd6e0c7a
companion <pandonautical@gmail.com>
2020-11-25 16:32:32 +0000

    what is going on

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express= require('express')
 var app = express();
 const fs = require('fs')
-const command = fs.readFileSync("./command.js", "utf-8")
+const command = fs.readFileSync("./little-api-companion/command.js", "utf-8")
 app.get('/command', (req, res) => {
   const thingy = command.split("\n")
   res.send(thingy.join("\n"));
