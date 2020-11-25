ad76369971f30b9a1f7ad647d54f6a55df81f7d1
companion <pandonautical@gmail.com>
2020-11-25 16:52:13 +0000

    hmmmmmmmmmmm

--- little-api-companion/command.js
+++ little-api-companion/command.js
@@ -1,10 +1,10 @@
-const mold(message, prefix) {
+const mold = (message, prefix) => {
   args = message.content.slice(prefix.length).trim().split(/ +/);
   command = args.shift().toLowerCase();
   molded = { "args" : args,  "command" : command };
   return molded;
 }
-const cbl(path, value) {
+const cbl = (path, value) => {
   const b = fs.readFileSync(path, 'utf-8');
   const blacklist = b.split("\n");
   return blacklist.includes(value);
