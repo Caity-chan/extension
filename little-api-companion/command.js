7e67ce99b6d6602f0bfa8c710532682ce60e46f2
companion <pandonautical@gmail.com>
2020-11-25 16:42:16 +0000

    will this work?

--- little-api-companion/command.js
+++ little-api-companion/command.js
@@ -1,23 +1,15 @@
-//const { cbl } = require('./blacklist.js')
 
-module.exports = {
-  //name: "command",
-  //description: "command api",
   mold(message, prefix) {
     args = message.content.slice(prefix.length).trim().split(/ +/);
     command = args.shift().toLowerCase();
     molded = { "args" : args,  "command" : command };
     return molded;
   }
-}
 
 
-module.exports = {
-  //name: "blacklist",
-  //description: "blacklist api",
+
   cbl(path, value) {
     const b = fs.readFileSync(path, 'utf-8');
     const blacklist = b.split("\n");
     return blacklist.includes(value);
   }
-}
