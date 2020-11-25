2588eed4f773015545c9675b1d1323bd2f21d65c
companion <pandonautical@gmail.com>
2020-11-25 16:25:11 +0000

    fasd

--- little-api-companion/blacklist.js
+++ little-api-companion/blacklist.js
@@ -2,7 +2,7 @@
 module.exports = {
   name: "blacklist",
   description: "blacklist api",
-  cbl: (path, value) => {
+  cbl(path, value) {
     const b = fs.readFileSync(path, 'utf-8');
     const blacklist = b.split("\n");
     return blacklist.includes(value);
