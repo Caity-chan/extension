f7520d4780abe28ca554039a44aaa8d2833549d0
companion <pandonautical@gmail.com>
2020-11-25 07:32:15 +0000

    d

--- little-api-companion/command.js
+++ little-api-companion/command.js
@@ -6,5 +6,10 @@ module.exports = {
     command = args.shift().toLowerCase();
     molded = { "args" : args,  "command" : command };
     return molded;
+  },
+  cbl(path, value) {
+    const b = fs.readFileSync(path, 'utf-8');
+    const blacklist = b.split("\n");
+    return blacklist.includes(value);
   }
 }
